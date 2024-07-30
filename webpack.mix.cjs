5f43d11f37ac4cae0179959ac37df72655f8a0de
coder4434 <tumerabdurrahman31@gmail.com>
2024-07-30 20:39:23 +0300

    day 3

--- webpack.mix.cjs
+++ webpack.mix.cjs
@@ -1,8 +1,7 @@
 const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
-   .css('resources/css/app.css', 'public/css'); // Sadece CSS dosyanız varsa
-
+   .sass('resources/sass/app.scss', 'public/css');
 
 if (mix.inProduction()) {
     mix.version();
